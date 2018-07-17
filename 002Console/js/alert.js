09a85b3744a891d47569daa962f7199c15f87c00
ywh3212 <627793119@qq.com>
2018-07-17 23:17:57 +0800

    modify

---
+++ 002Console/js/alert.js
@@ -0,0 +1,14 @@
+function woDeTaLu(){
+    alert("你猜一会儿会发生什么");
+    alert("再等等");
+    alert("也许真有奇迹");
+}
+alert("你怎么打开这个网页")
+woDeTaLu();
+alert("你相信奇迹吗?");
+alert("让我来向你提个问题");
+woDeTaLu();
+alert("果然没有奇迹");
+alert("我们就这样放弃吗");
+woDeTaLu();
+alert("这是最后一句了，88");
